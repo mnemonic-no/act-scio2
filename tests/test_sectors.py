aa9b2cf50c56720e5203bd0bc0291a83fabdd7f8
Geir Skjotskift <geir@mnemonic.no>
2020-03-16 14:02:25 +0100

    extract sectors from a body of text (#12)
    * extract sectors from a body of text
    
    * config change and remove monkeypatch
    
    * change in config directory resolve
    * removed monkeypatch

---
+++ tests/test_sectors.py
@@ -0,0 +1,25 @@
+
+from act.scio.attrdict import AttrDict
+from act.scio.plugins import sectors
+import os
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_sectors() -> None:
+    """ test for plugins """
+
+    prior_result = AttrDict()
+    prior_result.pos_tag = AttrDict()
+    prior_result.pos_tag.tokens = [('The', 'DT'), ('companies', 'NNS'), ('in', 'IN'), ('the', 'DT'), ('Bus', 'NNP'), (';', ':'), ('Finanical', 'NNP'), (',', ','), ('Aviation', 'NNP'), ('and', 'CC'), ('Automobile', 'NNP'), ('industry', 'NN'), ('is', 'VBZ'), ('in', 'IN'), ('trouble', 'NN'), ('.', '.')]
+
+    test_text = 'The companies in the Bus; Finanical, Aviation and Automobile industry are large.'
+
+    plugin = sectors.Plugin()
+    plugin.configdir = os.path.join(os.path.dirname(__file__), "../act/scio/etc/plugins")
+    res = await plugin.analyze(test_text, prior_result)
+
+    assert 'aerospace' in res.result.sectors
+    assert 'automotive' in res.result.sectors
+    assert 'finanical' not in res.result.sectors
+    assert 'finanical' not in res.result.unknown_sectors
