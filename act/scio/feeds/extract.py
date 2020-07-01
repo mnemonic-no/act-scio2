c5e8671ec1db6e9142c8916782f6e2e0cc16a231
Geir Skjotskfit <geir@mnemonic.no>
2020-07-01 15:15:02 +0200

    caching helper for use in upload code

---
+++ act/scio/feeds/extract.py
@@ -0,0 +1,151 @@
+"""Helper function for data extraction"""
+
+from typing import Dict, Text, Tuple, Optional, List
+import logging
+import argparse
+import os.path
+
+from bs4 import BeautifulSoup
+import html
+import justext
+import requests
+
+from act.scio.feeds import download
+
+LOGGER = logging.getLogger('root')
+
+
+def get_content_from_entry(entry: Dict) -> Text:
+    """Extract and concatenate the content portion of a feed entry"""
+
+    if "content" in entry:
+        return "\n".join([x["value"] for x in entry['content']])
+
+    LOGGER.warning("No content for %s", entry['link'])
+
+    return ""
+
+
+def get_summary_from_entry(entry: Dict) -> Text:
+    """Extract summary from feed entry"""
+
+    if "summary_detail" in entry:
+        value: Text = entry['summary_detail']['value']
+        return value
+    return ""
+
+
+def create_html(entry: Dict) -> Text:
+    """Wrap an entry in html headers and footers"""
+
+    html_data = """<html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        <p>
+        {summary}
+        </p>
+        {content}
+    </body>
+</html>"""
+
+    content = get_content_from_entry(entry)
+    summary = get_summary_from_entry(entry)
+
+    return html_data.format(title=entry["title"],
+                            content=content,
+                            summary=summary)
+
+
+def safe_filename(path: Text) -> Text:
+    """Make filename safe by only allowing alpha numeric characters,
+    digits and ._-"""
+
+    def _safe_char(c: Text) -> Text:
+        if c.isalpha():
+            return c
+        if c.isdigit():
+            return c
+        if c in "_-.":
+            return c
+        if c in " \t":
+            return "_"
+        return ""
+
+    return "".join(_safe_char(c) for c in path)
+
+
+def partial_entry_text_to_file(
+        args: argparse.Namespace,
+        entry: Dict) -> Tuple[Optional[Text], Optional[Text]]:
+    """Download the original content and write it to the proper file.
+    Return the html."""
+
+    if "link" not in entry:
+        LOGGER.warning("entry does not contain 'link'")
+        return None, None
+
+    url = entry["link"]
+
+    req = requests.get(url, headers=download.default_headers(), verify=False, timeout=60)
+
+    LOGGER.warning("Unable to download contnet: %s", url)
+    if req.status_code >= 400:
+        return None, None
+
+    filename = safe_filename(entry['title'])
+
+    html_data = "<html_data>\n<head>\n"
+    html_data += "<title>{0}</title>\n</head>\n".format(entry['title'])
+    html_data += "<body>\n"
+
+    raw_html = req.text
+
+    paragraphs = justext.justext(raw_html, justext.get_stoplist('English'))
+    for para in paragraphs:
+        if not para.is_boilerplate:
+            if para.is_heading:
+                html_data += "\n<h1>{0}</h1>\n".format(html.escape(para.text))
+            else:
+                html_data += "<p>\n{0}\n</p>\n".format(html.escape(para.text))
+
+    html_data += "\n</body>\n</html_data>"
+
+    full_filename = os.path.join(args.store_path, "download", filename + ".html")
+    with open(full_filename, "w") as html_file:
+        html_file.write(html_data)
+
+    # we want to return the raw_html and not the "article extraction"
+    # since we want to extract links to .pdfs etc.
+    return filename, raw_html
+
+
+def entry_text_to_file(
+        args: argparse.Namespace,
+        entry: Dict) -> Tuple[Optional[Text], Optional[Text]]:
+    """Extract the entry content and write it to the proper file.
+    Return the wrapped HTML"""
+
+    filename = safe_filename(entry['title'])
+
+    html_data = create_html(entry)
+
+    full_filename = os.path.join(os.path.join(args.store_path, "download"), filename + ".html")
+    with open(full_filename, "w") as html_file:
+        html_file.write(html_data)
+
+    return filename, html_data
+
+
+def get_links(entry: Dict, html_data: Text) -> List[Text]:
+    """Extract any links from the html"""
+
+    links = []
+    soup = BeautifulSoup(html_data, "html.parser")
+    if soup:
+        links = [a['href'] for a in soup.findAll('a', href=True)]
+    else:
+        LOGGER.warning("soup is none : %s", entry['title'])
+
+    return links
