01477c0ec4b4354f1ef8e28383bd17e84e66ccef
Geir Skjotskfit <geir@mnemonic.no>
2020-07-07 14:26:09 +0200

    Changed default for feed file
    -removed absolute path in default
    -fixed help text for ignore file default

--- act/scio/feeds/conf.py
+++ act/scio/feeds/conf.py
@@ -23,10 +23,9 @@ def get_args() -> argparse.Namespace:
         help="Location for stored files. Default = ~/.cache/scio-feeds (honors $XDG_CACHE_HOME")
     parser.add_argument("-v", "--verbose", action="store_true", help="Log level DEBUG")
     parser.add_argument("--debug", action="store_true", help="Log level DEBUG")
-    parser.add_argument("--ignore", type=str,
-                        help="file with ignore patterns (default: /opt/scio_feeds/ignore.txt)")
-    parser.add_argument("--feeds", default="/opt/scio_feeds/feeds.txt", type=str,
-                        help="feed urls (one pr. line) (default: /opt/scio_feeds/feeds.txt)")
+    parser.add_argument("--ignore", type=str, help="file with ignore patterns")
+    parser.add_argument("--feeds", default="feeds.txt", type=str,
+                        help="feed urls (one pr. line) (default: feeds.txt)")
     parser.add_argument("--cache", default="cache.db", help="sqlite db containing cached hashes")
     parser.add_argument("--scio", default=None, help="Upload to scio engine API url")
     parser.add_argument('--logfile')
