5a8b02db3b2c8e64567a792ce74435ddd34b97c5
Geir Skjotskfit <geir@mnemonic.no>
2020-09-02 10:22:01 +0200

    finding actors based on sentences

---
+++ act/scio/plugins/threatactor_nlp.py
@@ -0,0 +1,81 @@
+import addict
+from act.scio.vocabulary import Vocabulary
+from act.scio.plugin import BasePlugin, Result
+from typing import Text, List, Set
+import configparser
+import nltk
+import nltk.stem
+import os
+
+
+class Plugin(BasePlugin):
+    name = "nlp_actors"
+    info = "Extract actors based on language from a body of text"
+    version = "0.1"
+    dependencies: List[Text] = ["pos_tag"]
+
+    async def analyze(self, nlpdata: addict.Dict) -> Result:
+
+        res = addict.Dict()
+
+        threat_stem_postfix = {
+            'threat',     # threat
+            'crimin',     # criminal, criminals
+            'crime',      # crime
+            'espionage',  # espionage
+            'hack',       # hack, hacking,
+            'hacker',     # hacker, hackers
+            'crack',      # cracking, crack
+            'cracker',    # cracker, crackers
+            'terrorist'   # terrorist, terrorists
+        }
+
+        group_stem_postfix = {
+            'group',  # group, groups
+            'actor',  # actor, actors
+            'unit',   # unit, untis
+            'agent',  # agent, agents
+            'organ'   # organization, organizations
+        }
+
+        false_positive_filter = ["top",
+                                 "unknown",
+                                 "cyber"]  # "top threat groups", "cyber threat actors" etc...
+
+        posible_tag_types = {"NNP", "NNPS", "NN", "NNS", "JJ", "JJS"}
+        chain_tags = {",", ":", "CC"}
+        lookbefore_tags: Set[Text] = set()
+        lookbefore_tags.update(chain_tags)
+        lookbefore_tags.update(posible_tag_types)
+
+        ps = nltk.stem.PorterStemmer()
+
+        first_stage_found = False
+
+        pos_actors: List[Text] = []
+        # Look through all tokens. If any token relating to a threat actors is
+        # found, look-before and collect all nouns while the tokens are nouns
+        # or part of a listing.
+        for i, (token, tag) in enumerate(nlpdata.pos_tag.tokens):
+            if first_stage_found and tag in posible_tag_types and ps.stem(token) in group_stem_postfix:
+                n = i - 1
+                while nlpdata.pos_tag.tokens[n][1] in lookbefore_tags:
+                    n -= 1
+
+                current_actor: List[Text] = []
+                for (token, pos_tag) in nlpdata.pos_tag.tokens[n:i-1]:
+                    if pos_tag in chain_tags:
+                        pos_actors.append(" ".join(current_actor))
+                        current_actor = []
+                    elif pos_tag in posible_tag_types:
+                        if token in false_positive_filter:
+                            print("continue", token, pos_tag)
+                            continue
+                        current_actor.append(token)
+                pos_actors.append(" ".join(current_actor))
+
+            first_stage_found = bool(tag in posible_tag_types and
+                                     ps.stem(token) in threat_stem_postfix)
+
+        res.actors = pos_actors
+        return Result(name=self.name, version=self.version, result=res)
