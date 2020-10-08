eb020e77627223759401393a2b32c8a9628c4a73
Geir Skjotskfit <geir@mnemonic.no>
2020-10-08 16:32:26 +0200

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), "rb") as f:
 
 setup(
     name="act-scio",
-    version="0.0.31",
+    version="0.0.32",
     author="mnemonic AS",
     zip_safe=True,
     author_email="opensource@mnemonic.no",
