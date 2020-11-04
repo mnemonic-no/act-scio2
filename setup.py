c2ce539ed4b41f0ef750acf840972f2fa67a8fe8
Fredrik Borg <fredrikb@mnemonic.no>
2020-11-04 12:03:16 +0100

    add https://feeds.feedburner.com/TroyHunt to feeds.txt

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), "rb") as f:
 
 setup(
     name="act-scio",
-    version="0.0.33",
+    version="0.0.34",
     author="mnemonic AS",
     zip_safe=True,
     author_email="opensource@mnemonic.no",
