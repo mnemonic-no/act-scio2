e12121fc6d16a6cb95e39aa3c5f281cd1bc32315
Fredrik Borg <fredrikb@mnemonic.no>
2020-11-06 10:38:06 +0100

    run async_main as task

--- act/scio/analyze.py
+++ act/scio/analyze.py
@@ -188,7 +188,8 @@ def main() -> None:
     """Main entry point"""
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait([async_main()]))
+    task = loop.create_task(async_main())
+    loop.run_until_complete(task)
 
 
 if __name__ == "__main__":
