1c2a788c35b62a70ddcfcf4f314ce21fb811272f
Geir Skjotskift <geir@mnemonic.no>
2021-03-11 10:19:37 +0100

    Moved abbriviation to config. Added support for fe naming pattern (#69)
    * Moved abbriviation to config. Added support for fe naming pattern
    
    * test for uppercase_abbr in ini
    
    * check for empty strings in abbr list

--- act/scio/plugins/threatactor_pattern.py
+++ act/scio/plugins/threatactor_pattern.py
@@ -7,12 +7,27 @@ import configparser
 import os.path
 
 
-def normalize_ta(name: Text) -> Text:
-    return normalize(
+def normalize_ta(name: Text, uppercase_abbr: List[Text]) -> Text:
+    """Normalize TA names. Words are capitalized unless configured
+    to be excempt"""
+
+    res: Text = normalize(
         name,
         capitalize=True,
-        uppercase_abbr=["APT", "BRONZE", "IRON", "GOLD"],
+        uppercase_abbr=uppercase_abbr,
     )
+
+    return res
+
+
+def abbreviation_list(abbs: Text) -> List[Text]:
+    """Split the abbrivation string on |, and return a list of
+    abbreviations"""
+
+    uppercase_abbr = [x.strip() for x in abbs.split("|")]
+    uppercase_abbr = list(filter(bool, uppercase_abbr))
+
+    return uppercase_abbr
 
 
 class Plugin(BasePlugin):
@@ -27,13 +42,15 @@ class Plugin(BasePlugin):
         ini.read([os.path.join(self.configdir, "threatactor_pattern.ini")])
         ini['threat_actor']['alias'] = os.path.join(self.configdir, ini['threat_actor']['alias'])
 
+        uppercase_abbr = abbreviation_list(ini['threat_actor'].get('uppercase_abbr', ""))
+
         vocab = Vocabulary(ini['threat_actor'])
 
         res = addict.Dict()
 
         res.ThreatActors = vocab.regex_search(
             nlpdata.content,
-            normalize_result=normalize_ta,
+            normalize_result=(lambda x: normalize_ta(x, uppercase_abbr)),
             debug=self.debug)
 
         return Result(name=self.name, version=self.version, result=res)
--- tests/test_threatactor_pattern.py
+++ tests/test_threatactor_pattern.py
@@ -11,7 +11,7 @@ async def test_threatactor_pattern() -> None:
 
     nlpdata = addict.Dict()
 
-    nlpdata.content = '''Lorem Ipsum Dirty Panda, APT1 APT-2, Apt 35, APT_46'''
+    nlpdata.content = '''Lorem Ipsum Dirty Panda, APT1 APT-2, Apt 35, APT_46, UNC2354'''
 
     plugin = threatactor_pattern.Plugin()
     plugin.configdir = os.path.join(os.path.dirname(__file__), "../act/scio/etc/plugins")
@@ -22,3 +22,4 @@ async def test_threatactor_pattern() -> None:
     assert 'APT 2' in res.result.ThreatActors
     assert 'APT 35' in res.result.ThreatActors
     assert 'APT 46' in res.result.ThreatActors
+    assert 'UNC 2354' in res.result.ThreatActors
